616971ebd82ee9369299a01877ce40ced3224e8e
UshanKaushalya <Ushanwk22@gmail.com>
2023-04-05 00:51:21 +0530

    Login page initiate

--- src/lk/ijse/hibernate/d24/controller/LoginFormController.java
+++ src/lk/ijse/hibernate/d24/controller/LoginFormController.java
@@ -1,4 +1,23 @@
 package lk.ijse.hibernate.d24.controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginFormController {
+    public AnchorPane primaryPane;
+    public JFXTextField txtFldUsername;
+    public JFXTextField txtFldPassword;
+
+    public void btnSignInOnActiom(ActionEvent actionEvent) {
+        if(txtFldUsername.getText() .equals("user") && txtFldPassword.getText() .equals("1234")){
+            System.out.println("Done");
+        }else{
+            System.out.println("Not Done");
+        }
+    }
+
+    public void btnCloseOnAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
